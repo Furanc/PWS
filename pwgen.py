80e26f5305f7192fe5b05160dcd3228d4315b938
Didier de Weille <217214@leerling.alkwin.nl>
2022-11-24 17:11:38 +0100

    Output pwgen naar txt + renames
    Co-authored-by: Furanc <Furanc@users.noreply.github.com>

--- pwgen.py
+++ pwgen.py
@@ -6,11 +6,16 @@ import string
 
 alphabet = string.ascii_letters + string.digits
 
-n=0
-while n <= 10:
-    password = ''.join(secrets.choice(alphabet) for i in range(16))
-    print(password)
-    n = n+1
+def dataset():
+    n=0
+    while n <= 10:
+        password = ''.join(secrets.choice(alphabet) for i in range(16))
+        print(password)
+        with open('dataset.txt','a') as text_file:
+            text_file.write(str(password) + '\n')
+            text_file.close()
+        n = n+1    
+    
 
 
-
+dataset()
