86089064e3c0faa81f08e39a0b77e886490963df
Franck Leijen <fcleijen@gmail.com>
2022-11-26 15:07:26 +0100

    aa Co-authored-by: Didier de Weille <DatDier@users.noreply.github.com>

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ import secrets
 import string
 
 alphanumeric = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24, 'z': 25, 'A': 26, 'B': 27, 'C': 28, 'D': 29, 'E': 30, 'F': 31, 'G': 32, 'H': 33, 'I': 34, 'J': 35, 'K': 36, 'L': 37, 'M': 38, 'N': 39, 'O': 40, 'P': 41, 'Q': 42, 'R': 43, 'S': 44, 'T': 45, 'U': 46, 'V': 47, 'W': 48, 'X': 49, 'Y': 50, 'Z': 51, '0': 52, '1': 53, '2': 54, '3': 55, '4': 56, '5': 57, '6': 58, '7': 59, '8': 60, '9': 61}
-inv_alphanumeric = {v: k for k, v in alphanumeric.items()}
-alphabet = string.ascii_letters + string.digits
+inv_alphanumeric, alphabet = {v: k for k, v in alphanumeric.items()}, string.ascii_letters + string.digits
 
 def pwgen():
     global password
@@ -25,11 +24,9 @@ def keygen():
     return keylist
 
 def encrypt():
-    global pwlen, keyi, cipher
-    pwi, keyi = pwgen(), keygen()
-    cipher = []
+    global pwi, keyi, pwlen, cipher
+    pwi, keyi, cipher = pwgen(), keygen(), []
     pwlen = len(pwi)
-    i = 0
     for i in range(pwlen):
         ciphernum = (int((alphanumeric[keyi[i]] + alphanumeric[pwi[i]])) % 62)
         cipherchar = inv_alphanumeric[ciphernum]
@@ -39,10 +36,7 @@ def encrypt():
     return cipher
 
 def decrypt(): 
-    listc = encrypt()
-    plaintext = []
-
-    i = 0
+    listc, plaintext = encrypt(), []
     for i in range(pwlen):
         pwnum = (int((alphanumeric[listc[i]] - alphanumeric[keyi[i]])) % 62)
         pwchar = inv_alphanumeric[pwnum]
