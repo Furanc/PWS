7cff853739e7962c26e650b100202fb6e4652f97
Franck Leijen <fcleijen@gmail.com>
2022-11-27 14:05:08 +0100

    meer onnodige kleine changes

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-#import math
-#import numpy as np
-#import os
 import secrets
 import string
 
-
-#alphanumeric = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24, 'z': 25, 'A': 26, 'B': 27, 'C': 28, 'D': 29, 'E': 30, 'F': 31, 'G': 32, 'H': 33, 'I': 34, 'J': 35, 'K': 36, 'L': 37, 'M': 38, 'N': 39, 'O': 40, 'P': 41, 'Q': 42, 'R': 43, 'S': 44, 'T': 45, 'U': 46, 'V': 47, 'W': 48, 'X': 49, 'Y': 50, 'Z': 51, '0': 52, '1': 53, '2': 54, '3': 55, '4': 56, '5': 57, '6': 58, '7': 59, '8': 60, '9': 61}
-#inv_alphanumeric,  = {v: k for k, v in alphanumeric.items()}, 
 alphabet = string.ascii_letters + string.digits
 
 def pwgen():
@@ -97,4 +91,5 @@ if __name__ == '__main__':
     # with open('dataset.txt','a') as text_file:
     #     text_file.write(f'''{password}, {key}, {''.join(vig)}, {decrypted} \n''')
     #     text_file.close()
-    print(f'Your password was: {vig_decrypt()}')
+    for n in range(1):
+        print(f'Your password was: {vig_decrypt()}')
